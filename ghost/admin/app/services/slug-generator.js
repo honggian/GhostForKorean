70f96a2e637936c6e9f80aeedc43e7c81e5a9f55
honggian <honggian@honggian.com>
2024-06-09 12:14:02 +0900

    URL for Korean language

--- ghost/admin/app/services/slug-generator.js
+++ ghost/admin/app/services/slug-generator.js
@@ -1,7 +1,6 @@
 import RSVP from 'rsvp';
 import Service, {inject as service} from '@ember/service';
 import classic from 'ember-classic-decorator';
-import {slugify} from '@tryghost/string';
 
 const {resolve} = RSVP;
 
@@ -17,8 +16,7 @@ export default class SlugGeneratorService extends Service {
             return resolve('');
         }
 
-        // We already do a partial slugify at the client side to prevent issues with Pro returning a 404 page because of invalid (encoded) characters (a newline, %0A, for example)
-        url = this.get('ghostPaths.url').api('slugs', slugType, encodeURIComponent(slugify(textToSlugify)));
+        url = this.get('ghostPaths.url').api('slugs', slugType, encodeURIComponent(textToSlugify));
 
         return this.ajax.request(url).then((response) => {
             let [firstSlug] = response.slugs;
